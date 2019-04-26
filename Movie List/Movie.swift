4583a66d4c09a83c60d3df2a3130cf916179d40a
Alex <alexcc17@gmail.com>
2019-04-26 19:58:48 -0400

    Still building...

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Alex on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Equatable {
+    var movieTitle: String
+    var seen: Bool = false
+}
