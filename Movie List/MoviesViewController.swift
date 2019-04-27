71c98ae53168d0fa37cde0c064b576486e34109a
Alex <alexcc17@gmail.com>
2019-04-26 21:05:12 -0400

    Remove unnecessary variables

--- Movie List/MoviesViewController.swift
+++ Movie List/MoviesViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class MoviesViewController: UIViewController {
 
-    @IBOutlet var movieNameLbl: UILabel!
-    @IBAction func seenBtnPressed(_ sender: UIButton) {
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
